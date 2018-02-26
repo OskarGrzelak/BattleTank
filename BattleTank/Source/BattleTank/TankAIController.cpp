604029704e8a24ade2c04db32eb0fd054216e2b3
Oskar-Komputer\Oskar <oskargrzelak@gmail.com>
2018-02-26 10:40:27 +0100

    Looking for player controlled tank by AI tanks

---
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "Engine/World.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("AI cannot find player tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s found a player tank: %s"), *(GetAIControlledTank()->GetName()), *(PlayerTank->GetName()));
+	}
+}
+
+ATank * ATankAIController::GetAIControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank * ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+	return Cast<ATank>(PlayerPawn);
+}
--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -2,6 +2,19 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	if (!GetControlledTank())
+	{
+		UE_LOG(LogTemp, Error, TEXT("Player does not control a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("player controlls: %s"), *(GetControlledTank()->GetName()));
+	}
+}
+
 ATank * ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
